8cd4ba517836e55cfaaf46850f4ed71384b8ddc6
a8613 <54513084+a8613@users.noreply.github.com>
2021-06-15 16:45:19 -0400

    Update create_response.py

--- create_response.py
+++ create_response.py
@@ -1,4 +1,15 @@
 import json
 import boto3
 def lambda_handler(event, context):
+  # Instanciating connection objects with DynamoDB using boto3 dependency
+    dynamodb = boto3.resource('dynamodb')
+    client = boto3.client('dynamodb')
+    
+    # Getting the table the table questions object
+    questions_table = dynamodb.Table('questions')
+    
+    # current datetime as a potential timestamp attribute
+    # event_timestamp = (datetime.now()).strftime("%Y-%m-%d %H:%M:%S")
+    question_id = event['resource']
+    responses = table.query(KeyConditionExpression=Key('question_id').eq(question_id))
   return
