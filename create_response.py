02afe2ca59ef1a65790e9ee6b7da3270e785eb6f
a8613 <54513084+a8613@users.noreply.github.com>
2021-06-15 20:18:10 -0400

    Avi's Changes

--- create_response.py
+++ create_response.py
@@ -1,4 +1,20 @@
 import json
 import boto3
 def lambda_handler(event, context):
-  return
+  # Instanciating connection objects with DynamoDB using boto3 dependency
+    dynamodb = boto3.resource('dynamodb')
+    client = boto3.client('dynamodb')
+    
+    # Getting the table the table questions object
+    questions_table = dynamodb.Table('questions')
+    
+    # capturing question_id and returning responses for that question
+    question_id = event['resource']
+    question = questions_table.get_item(
+           Key={
+               'question_id': question_id
+           }
+        )   
+    responses = question['responses']
+    
+  return responses
